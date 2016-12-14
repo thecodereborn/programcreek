780d554f950bb624311694422544d8dbbb21d841
yash mahajan <thecodereborn@gmail.com>
2016-12-14 08:46:29 +0530

    Create Rotate Array.cpp

---
+++ StringArray/Rotate Array.cpp
@@ -0,0 +1,12 @@
+# programcreek
+http://www.programcreek.com/2012/11/top-10-algorithms-for-coding-interview/
+
+http://www.programcreek.com/2015/03/rotate-array-in-java/
+
+Rotate an array of n elements to the right by k steps.
+
+For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4]. How many different ways do you know to solve this problem?
+
+
+1.  The problem can be solved by dividing array in two array .. the first array is of size n-k and the second array of the remaining size
+	now merge the two array in reverse order( meaning second array + first array)
